645ef9da33e7eb1bc8ee9070e8330e1447cb41cc
Saeed Jaffer <ST10106174@vcconnect.edu.za>
2022-04-25 14:30:40 +0200

    init commit

---
+++ src/testingmessage/TestingMessage.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testingmessage;
+
+/**
+ *
+ * @author ST10106174
+ */
+public class TestingMessage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
